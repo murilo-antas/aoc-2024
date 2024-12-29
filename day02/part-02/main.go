3848dec6a39c72ec75722b09ecfcfe92fa14fc28
Murilo Antas Pereira <murilo.antas@gmail.com>
2024-12-28 22:40:34 -0300

    day 02 part 2

---
+++ day02/part-02/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"util"
+)
+
+const (
+	MinDiff = 1
+	MaxDiff = 3
+)
+
+func main() {
+	reports, err := readLevels("../input.txt")
+	util.Check(err)
+
+	safeReports := 0
+	for _, report := range reports {
+		fmt.Println(report)
+		if checkSafeReport(report) || checkSafeReportWithToleration(report) {
+			safeReports += 1
+		}
+	}
+	fmt.Println("Safe reports:", safeReports)
+}
+
+func checkSafeReportWithToleration(report []int) bool {
+	for index := range report {
+		if checkSafeReport(removeIndex(report, index)) {
+			return true
+		}
+	}
+	return false
+}
+
+func removeIndex(s []int, index int) []int {
+	ret := make([]int, 0)
+	ret = append(ret, s[:index]...)
+	return append(ret, s[index+1:]...)
+}
+
+func checkSafeReport(report []int) bool {
+	lastValue := report[0]
+	increasing := report[0]-report[1] >= 0
+	for index := 1; index < len(report); index++ {
+		diff := lastValue - report[index]
+		if diff == 0 {
+			return false
+		}
+
+		if increasing && (diff < MinDiff || diff > MaxDiff) {
+			return false
+		}
+
+		if !increasing && (diff > -MinDiff || diff < -MaxDiff) {
+			return false
+		}
+
+		lastValue = report[index]
+	}
+	return true
+}
+
+func readLevels(fileName string) ([][]int, error) {
+	file, err := os.Open(fileName)
+	util.Check(err)
+	defer file.Close()
+
+	var reports [][]int
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		numbers := strings.Split(line, " ")
+		var report []int
+		for _, value := range numbers {
+			level, err := strconv.Atoi(value)
+			util.Check(err)
+			report = append(report, level)
+		}
+		reports = append(reports, report)
+	}
+	return reports, scanner.Err()
+}
